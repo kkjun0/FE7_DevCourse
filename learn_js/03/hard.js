a391f2fb51b82501c00cf1da6e06e7b0f8f800be
kimjunyeong123 <wns13255@naver.com>
2025-08-07 18:33:51 +0900

    add hard 6

--- learn_js/03/hard.js
+++ learn_js/03/hard.js
@@ -21,35 +21,41 @@
 //   if (count < 2 && start !== 1) results.push(start);
 //   start++;
 // }
-
 // console.log(results); // [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
 // // 3
 // results2 = 0;
 // const numbers = [5, 10, 15, 20, 25]; // 배열
-
 // for (v of numbers) {
 //   results2 += v;
 // }
-
 // console.log(results2); // 75
 
 // // 4
 // const str = 'Hello, World!';
 // let reversed = '';
-
 // for (let i = str.length - 1; i >= 0; i--) {
 //   reversed += str[i];
 // }
-
 // console.log('뒤집힌 문자열:', reversed); // "!dlroW ,olleH"
 
 // // 5
 // let n = 5; // 예시 숫자
 // let factorial = 1;
-
 // for (let i = n; i >= 1; i--) {
 //   factorial *= i;
 // }
-
 // console.log(`${n}의 팩토리얼:`, factorial); // 출력 120
+
+// // 6.
+// let units,
+//   tens,
+//   hundreds = 0;
+
+// for (let i = 100; i <= 999; i++) {
+//   units = i % 10;
+//   tens = ((i % 100) - units) / 10;
+//   hundreds = (i - tens * 10 - units) / 100;
+//   let sum = units ** 3 + tens ** 3 + hundreds ** 3;
+//   if (i === sum) console.log(i);
+// }
